40cd3f125524f6adc14ddfe422be26012adcad1e
Mehmet <mmtkalayci@gmail.com>
2024-06-23 12:44:42 +0300

    mountWidget func added

--- src/main.js
+++ src/main.js
@@ -2,4 +2,10 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+//createApp(App).mount('#app')
+
+function mountWidget(el) {
+    createApp(App).mount(el);
+}
+
+window.mountWidget = mountWidget;
