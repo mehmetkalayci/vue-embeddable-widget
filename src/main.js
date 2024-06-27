59b9bcffda5bc7af8dc3749208bddcc891ae3fb2
Mehmet <mmtkalayci@gmail.com>
2024-06-27 16:06:48 +0300

    dev mode

--- src/main.js
+++ src/main.js
@@ -2,10 +2,14 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
-//createApp(App).mount('#app')
-
 function mountWidget(el) {
     createApp(App).mount(el);
 }
 
 window.mountWidget = mountWidget;
+
+const widgetMode = import.meta.env.VITE_WIDGET_MODE;
+
+if (widgetMode === 'development') {
+    createApp(App).mount('#app');
+}
